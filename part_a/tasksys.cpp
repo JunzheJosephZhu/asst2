b0a17d8b0bbb15d654345af48ed64666dff49c8a
JunzheJosephZhu <junzhe.joseph.zhu@gmail.com>
2022-10-24 23:24:05 -0700

    nothing

--- part_a/tasksys.cpp
+++ part_a/tasksys.cpp
@@ -214,77 +214,102 @@ TaskSystemParallelThreadPoolSleeping::TaskSystemParallelThreadPoolSleeping(int n
     //
     current_task = 0;
     num_total_tasks = 0;
-    threads = new std::thread[num_threads];
-    ready = new bool[num_threads];
-    for (int i=0; i<num_threads; i++) {
-        ready[i] = true;
+    quit = 0;
+    completed = new bool[num_threads];
+    for (int i=0; i<num_threads; i++){
+        completed[i] = false;
     }
+    quit = new bool[num_threads];
+    for (int i=0; i<num_threads; i++){
+        quit[i] = false;
+    }
+    threads = new std::thread[num_threads];
     for (int i=0; i<num_threads; i++) {
         threads[i] = std::thread(&TaskSystemParallelThreadPoolSleeping::collaborate, this, i);
     }
 }
 
 void TaskSystemParallelThreadPoolSleeping::collaborate(int thread_id) {
-    bool need_run;
+    // acquire lock 
+    std::unique_lock<std::mutex> lk(mutex_);
     int task;
     while (true) {
-        // quit if total task is -1
-        if (num_total_tasks == -1)
+        if (num_total_tasks == -1){ // quit if total task is -1
+            // printf("thread %d quit\n", thread_id);
+            lk.unlock();
+            quit[thread_id] = true;
             return;
-        // check if something needs to be done
-        mutex_.lock();
-        if (current_task < num_total_tasks){
-            task = current_task++;
-            need_run = true;
-        } else {
-            need_run = false;
-            ready[thread_id] = true;
         }
-        mutex_.unlock();
-        if (need_run) {
+        if (current_task < num_total_tasks){ // some work to do
+            task = current_task++;
+            // unlock. Do work. Lock again
+            lk.unlock();
+            condition_variable_.notify_all(); // notify all threads
             runnable->runTask(task, num_total_tasks);
+            lk.lock();
+        }
+        else{ // no work to do. wake master
+            completed[thread_id] = true;
+            condition_variable_.wait(lk);
         }
     }
 }
 
 
 void TaskSystemParallelThreadPoolSleeping::run(IRunnable* runnable, int num_total_tasks) {
+
+
     //
     // TODO: CS149 students will modify the implementation of this
-    // method in Part A.  The implementation provided below runs all
+    // method in Parts A and B.  The implementation provided below runs all
     // tasks sequentially on the calling thread.
     //
     this -> runnable = runnable;
     mutex_.lock();
-    this -> current_task = 0;
-    this -> num_total_tasks = num_total_tasks;
-    // in case threads can't set ready in time
-    for (int i=0; i<num_threads; i++) {
-        ready[i] = false;
+    current_task = 0;
+    for (int i=0; i<num_threads; i++){
+        completed[i] = false;
     }
+    this -> num_total_tasks = num_total_tasks;
     mutex_.unlock();
 
-    // check all is done
-    while (true){
-        bool all_done = true;
-        for (int i=0; i<num_threads; i++) {
-            if (!ready[i]) {
-                all_done = false;
+    condition_variable_.notify_all();
+
+    bool all_completed = false;
+    while(!all_completed){
+        all_completed = true;
+        for (int i=0; i<num_threads; i++){
+            if (!completed[i]){
+                all_completed = false;
                 break;
             }
-        }
-        if (all_done) {
-            break;
         }
     }
 }
 
+
 TaskSystemParallelThreadPoolSleeping::~TaskSystemParallelThreadPoolSleeping() {
+    //
+    // TODO: CS149 student implementations may decide to perform cleanup
+    // operations (such as thread pool shutdown construction) here.
+    // Implementations are free to add new class member variables
+    // (requiring changes to tasksys.h).
+    //
     num_total_tasks = -1;
+    bool all_quit = false;
+    while(!all_quit){
+        condition_variable_.notify_all();
+        all_quit = true;
+        for (int i=0; i<num_threads; i++){
+            if (!quit[i]){
+                all_quit = false;
+                break;
+            }
+        }
+    }
     for (int i=0; i<num_threads; i++)
         threads[i].join();
     delete[] threads;
-    delete[] ready;
 }
 
 
--- part_a/tasksys2.cpp
+++ part_a/tasksys2.cpp
@@ -214,102 +214,77 @@ TaskSystemParallelThreadPoolSleeping::TaskSystemParallelThreadPoolSleeping(int n
     //
     current_task = 0;
     num_total_tasks = 0;
-    quit = 0;
-    completed = new bool[num_threads];
-    for (int i=0; i<num_threads; i++){
-        completed[i] = false;
-    }
-    quit = new bool[num_threads];
-    for (int i=0; i<num_threads; i++){
-        quit[i] = false;
-    }
     threads = new std::thread[num_threads];
+    ready = new bool[num_threads];
+    for (int i=0; i<num_threads; i++) {
+        ready[i] = true;
+    }
     for (int i=0; i<num_threads; i++) {
         threads[i] = std::thread(&TaskSystemParallelThreadPoolSleeping::collaborate, this, i);
     }
 }
 
 void TaskSystemParallelThreadPoolSleeping::collaborate(int thread_id) {
-    // acquire lock 
-    std::unique_lock<std::mutex> lk(mutex_);
+    bool need_run;
     int task;
     while (true) {
-        if (num_total_tasks == -1){ // quit if total task is -1
-            // printf("thread %d quit\n", thread_id);
-            lk.unlock();
-            quit[thread_id] = true;
+        // quit if total task is -1
+        if (num_total_tasks == -1)
             return;
-        }
-        if (current_task < num_total_tasks){ // some work to do
+        // check if something needs to be done
+        mutex_.lock();
+        if (current_task < num_total_tasks){
             task = current_task++;
-            // unlock. Do work. Lock again
-            lk.unlock();
-            condition_variable_.notify_all(); // notify all threads
-            runnable->runTask(task, num_total_tasks);
-            lk.lock();
+            need_run = true;
+        } else {
+            need_run = false;
+            ready[thread_id] = true;
         }
-        else{ // no work to do. wake master
-            completed[thread_id] = true;
-            condition_variable_.wait(lk);
+        mutex_.unlock();
+        if (need_run) {
+            runnable->runTask(task, num_total_tasks);
         }
     }
 }
 
 
 void TaskSystemParallelThreadPoolSleeping::run(IRunnable* runnable, int num_total_tasks) {
-
-
     //
     // TODO: CS149 students will modify the implementation of this
-    // method in Parts A and B.  The implementation provided below runs all
+    // method in Part A.  The implementation provided below runs all
     // tasks sequentially on the calling thread.
     //
     this -> runnable = runnable;
     mutex_.lock();
-    current_task = 0;
-    for (int i=0; i<num_threads; i++){
-        completed[i] = false;
-    }
+    this -> current_task = 0;
     this -> num_total_tasks = num_total_tasks;
+    // in case threads can't set ready in time
+    for (int i=0; i<num_threads; i++) {
+        ready[i] = false;
+    }
     mutex_.unlock();
 
-    condition_variable_.notify_all();
-
-    bool all_completed = false;
-    while(!all_completed){
-        all_completed = true;
-        for (int i=0; i<num_threads; i++){
-            if (!completed[i]){
-                all_completed = false;
+    // check all is done
+    while (true){
+        bool all_done = true;
+        for (int i=0; i<num_threads; i++) {
+            if (!ready[i]) {
+                all_done = false;
                 break;
             }
+        }
+        if (all_done) {
+            break;
         }
     }
 }
 
-
 TaskSystemParallelThreadPoolSleeping::~TaskSystemParallelThreadPoolSleeping() {
-    //
-    // TODO: CS149 student implementations may decide to perform cleanup
-    // operations (such as thread pool shutdown construction) here.
-    // Implementations are free to add new class member variables
-    // (requiring changes to tasksys.h).
-    //
     num_total_tasks = -1;
-    bool all_quit = false;
-    while(!all_quit){
-        condition_variable_.notify_all();
-        all_quit = true;
-        for (int i=0; i<num_threads; i++){
-            if (!quit[i]){
-                all_quit = false;
-                break;
-            }
-        }
-    }
     for (int i=0; i<num_threads; i++)
         threads[i].join();
     delete[] threads;
+    delete[] ready;
 }
 
 
